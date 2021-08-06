d364be5b48d6a35f9fad5744a8ea4f569ff2a03d
paulinakoz <paulinakozaczek@gmail.com>
2021-08-06 17:23:31 +0100

    database table and migration

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
-    const orm = await MikroORM.init({
-        dbName: 'reddit',
-        type: 'postgresql',
-        debug: !__prod__,
-    });
+    const orm = await MikroORM.init(mikroConfig);
+    await orm.getMigrator().up();
+    const post = orm.em.create(Post, {title: "my first post"});
+    await orm.em.persistAndFlush(post);
 }
 
 main();
